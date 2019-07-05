365073bedf5cf15eec3ca5f5df70469f71cca8ed
sun-fox <1999sunnykumarsingh@gmail.com>
2019-07-05 16:30:46 +0530

    Added ability to import ".csv" files into database via the API.

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var express = require("express"),
   app = express(),
   mysql = require("mysql"),
+  fs = require("fs"),
+  parse = require("csv-parse"),
+  async = require("async"),
+  multer = require("multer"),
   bodyParser = require("body-parser"),
+  exec = require("child_process").exec,
   connection = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -9,6 +14,64 @@ var express = require("express"),
     database: "snapcart"
   });
 app.use(bodyParser.urlencoded({ extended: true }));
+
+//Constructing File Name
+var filename = Date.now() + ".csv";
+
+// Initializing Total CSV rows
+var total_rows = 0;
+
+// Initializing res.send Output Data
+var output_array = [];
+
+// Intializing product Availability
+var row_availability = 0;
+
+// Primary Key Colname Initialization
+var primary_key_colname = "";
+//The WHERE part of query
+var update_query_id="";
+// TO STORE ROW COUNT
+var row_counter = 0;
+
+// TO STORE COLUMN HEADER
+var col_header = [];
+
+//the input file name
+var client_tablename="";
+
+// File Upload Configs
+var storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, "uploads");
+  },
+  filename: function(req, file, cb) {
+    cb(null, filename);
+  }
+});
+
+// Initializing storage to multer
+var upload = multer({ storage: storage });
+
+// const express = require("express");
+// const app = express();
+const port = 3000;
+
+// Constructing complete file path
+var inputFile = "uploads/" + filename;
+
+app.post("/snapcart/import/:client_file_tablename", upload.single("filename"), function(
+  req,
+  res,
+  next
+) {
+  // Get CSV Row Count After Upload
+  countFileLines(inputFile);
+  row_counter=0;
+  client_tablename=req.params.client_file_tablename;
+  // Call Import Function
+  importData(req, res);
+});
 
 // Get Products and Search Products
 app.get("/snapcart", function(req, res) {
@@ -73,7 +136,131 @@ app.listen(3000, "localhost", function() {
 
 //MODULAR FUNCTIONS
 //Functions making code modular
+// CSV ROW COUNTER
+function countFileLines(filePath) {
+  return new Promise((resolve, reject) => {
+    let lineCount = 0;
+    fs.createReadStream(filePath)
+      .on("data", buffer => {
+        let idx = -1;
+        lineCount--; // Because the loop will run once for idx=-1
+        do {
+          idx = buffer.indexOf(10, idx + 1);
+          lineCount++;
+        } while (idx !== -1);
+        total_rows = lineCount;
+      })
+      .on("end", () => {
+        resolve(lineCount);
+      })
+      .on("error", reject);
+  });
+}
 
+// CSV DATA IMPORT FUNCTION
+function importData(req, res) {
+  var parser = parse({ delimiter: "," }, function(err, data) {
+    async.eachSeries(data, function(line, callback) {
+      // do something with the line
+      doSomething(line, req, res).then(function() {
+        // when processing finishes invoke the callback to move to the next one
+        callback();
+      });
+    });
+  });
+
+  fs.createReadStream(inputFile).pipe(parser);
+}
+
+// CALLBACK FUNCTION OF CSV ITERATION
+async function doSomething(line, req, res) {
+  if (row_counter === 0) {
+    col_header = line;
+  }
+  await row_counter++;
+  console.log(row_counter+" = "+line);
+  if (col_header.length != 0 && row_counter === 1) {
+    primary_key_colname = col_header[0];
+    // IGNORING THE COLUMN HEADER ROW
+  } else if(row_counter > 1) {
+    var inserter_query = insertQueryGenerator(line);
+    var updater_query = UpdateQueryGenerator(line);
+    UploaderLogicFunc(inserter_query,updater_query,line);
+    // output_array.push(insert_query);
+    // output_array.push(update_query + ";");
+    console.log(row_counter+" vs "+total_rows);
+    if (row_counter == total_rows) {
+        res.send("Import Completed Successfully!!");
+    //   res.send(output_array);
+    }
+  }
+}
+
+function UploaderLogicFunc(inserter_query,updater_query,line){
+  var id_extracter_query =
+      "SELECT COUNT(*) as row_count FROM "+client_tablename+" WHERE id=" + line[0];
+
+    connection.query(id_extracter_query, function(err, row) {
+      if (err) {
+        throw err;
+      }
+
+      row_availability = row[0]["row_count"];
+      console.log(row_availability);
+      if (row_availability === 1) {
+        // console.log(update_query);
+        connection.query(updater_query, function(err, rows) {
+          if (err) {
+            throw err;
+          }if (rows.affectedRows > 0) {
+            console.log("Row Updated");
+          } else {
+            res.send("Error!! Please try again.");
+          }
+        });
+      } else {
+        connection.query(inserter_query, function(err, rows) {
+          if (err) {
+            throw err;
+          }
+          if (rows.affectedRows > 0) {
+            console.log("Row Inserted");
+          } else {
+            res.send("Error!! Please try again.");
+          }
+        });
+      }
+    });
+}
+
+function insertQueryGenerator(line){
+  //CONSTRUCTING INSERT QUERY
+  var insert_query =
+  "INSERT INTO "+client_tablename+" (" +
+  (col_header.join(",")).split(/,(.+)/)[1] +
+  ") VALUES(" +
+  (line.map(line_row => `'${line_row}'`).join(",")).split(/,(.+)/)[1] +
+  ");";
+  console.log(insert_query);
+  return insert_query;
+}
+
+function UpdateQueryGenerator(line){
+//CONSTRUCTING UPDATE QUERY
+var update_query = "UPDATE testing_tbl SET ";
+//console.log(col_header);
+for (let col in col_header) {
+  if (col_header[col] != primary_key_colname) {
+    //console.log("col_header   "+col_header[col]);
+    update_query = update_query + col_header[col] + "='" + line[col] + "',";
+  } else {
+    update_query_id = " WHERE " + primary_key_colname + "=" + line[0];
+  }
+}
+update_query = update_query.slice(0, -1) + update_query_id+" ;";
+console.log(update_query);
+return update_query;
+}
 //gets the items present in the table
 function getAll(res,table_name){
   res.header("Access-Control-Allow-Origin", "*");
